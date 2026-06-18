b216b7f48ce927a4b8cf2ef0b2e6555be8c12888
pallavikanwate <kanwatepallavi4@gmail.com>
2026-06-18 16:49:35 +0530

    work on functionallity

--- assests/js/app.js
+++ assests/js/app.js
@@ -9,7 +9,6 @@ const updateBtn = document.getElementById('updateBtn')
 const postcontainer = document.getElementById('postcontainer')
 const spinner = document.getElementById('spinner')
 
-
 let base_url= "https://jsonplaceholder.typicode.com"; 
 let post_url =`${base_url}/posts`; 
 
@@ -23,10 +22,9 @@ function snackbar(msg,icon){
 function createcard(arr){
     let res=' ';
     arr.forEach(ele => {
-        res += `<div class="col-md-4 " id=${ele.id}>
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="card-header">
+        res += `<div class="col-md-4 my-4" id=${ele.id}>
+                    <div class="card h-100">
+                            <div class="card-header" data-toggle="tooltip" data-placement="top" title="${ele.title}">
                                 <h3>${ele.title}</h3>
                             </div>
                             <div class="card-body">
@@ -45,6 +43,7 @@ function createcard(arr){
 }
 
 function fetchposts(){
+    spinner.classList.remove('d-none')
     let xhr = new XMLHttpRequest()
     xhr.open('GET', post_url, true)
     xhr.send(null)
@@ -55,22 +54,19 @@ function fetchposts(){
         if(xhr.status>=200 && xhr.status <= 299){
             let data = JSON.parse(xhr.response)
             cl(data)
-
             createcard(data)
         }else{
             cl('api is failed')
         }
+         spinner.classList.add('d-none')
     }
 }
 fetchposts()
 
 
 function onPostSubmit(eve){ 
-    
-   
-
     eve.preventDefault(); 
-
+ spinner.classList.remove('d-none')
  let postObj = { 
            title:titlecontrol.value ,
            body:bodycontrol.value,
@@ -89,18 +85,18 @@ function onPostSubmit(eve){
               postform.reset();
          
            let col=document.createElement('div');
-               col.className  = 'col-md-4 mb-3'; 
+               col.className  = 'col-md-4 my-4'; 
                col.id         = resp.id;
-               col.innerHTML  = ` <div class="card">
-                                 <div class="card-header">
+               col.innerHTML  = ` <div class="card h-100">
+                                 <div class="card-header" data-toggle="tooltip" data-placement="top" title="${postObj.title}">
                                  <H3>${postObj.title} </H3>                           
                                   </div>
                                    <div class="card-body">
                                      ${postObj.body}
                                     </div>
                                  <div class="card-footer d-flex justify-content-between">
-                                  <button onclick="onEdit(this)" class="btn btn-inline-block btn-outline-primary">Edit</button>
-                                  <button onclick="onRemove(this)" class="btn btn-inline-block btn-outline-danger">Delete</button>
+                                  <button onclick="onEdit(this)" class="btn btn-inline-block btn-outline-primary btn-sm">Edit</button>
+                                  <button onclick="onRemove(this)" class="btn btn-inline-block btn-outline-danger btn-sm">Remove</button>
                                  </div>
                                 </div>` 
 
@@ -111,38 +107,45 @@ function onPostSubmit(eve){
             }else{ 
                    snackbar('New post is not created', 'error');   
                  }
-            } 
-
-
-        //  xhr.onerror = function (){  }   
+                  spinner.classList.add('d-none')
+            }
 
       } 
       
-function onRemove(ele){ 
-     let removeId =  ele.closest('.col-md-4').id ;
-     let removeUrl=  `${base_url}/posts/${removeId}`
-
-     let xhr= new XMLHttpRequest(); 
-         xhr.open('DELETE', removeUrl);
-         xhr.send(null);
-         
-         xhr.onload = function(){ 
-             if(xhr.status>=200 && xhr.status<=299){ 
-               ele.closest('.col-md-4').remove();
-             }else{
-                snackbar('Not  deleted','error')
-             } 
-         }
-      
-      } 
-
-
-
+function onRemove(ele){
+    Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+            }).then((result) => {
+                if (result.isConfirmed){
+                        spinner.classList.remove('d-none');
+                    let removeId =  ele.closest('.col-md-4').id ;
+                    let removeUrl=  `${base_url}/posts/${removeId}`
+                    let xhr= new XMLHttpRequest(); 
+                        xhr.open('DELETE', removeUrl);
+                        xhr.send(null);
+                
+                    xhr.onload = function(){ 
+                        if(xhr.status>=200 && xhr.status<=299){ 
+                        ele.closest('.col-md-4').remove();
+                        }else{
+                            snackbar('Not  deleted','error')
+                        } 
+                        spinner.classList.add('d-none')
+                    }
+        
+                } 
+            });
+}
 
 function onEdit(ele){ 
     let editId= ele.closest('.col-md-4').id ;
     localStorage.setItem('EditId', editId);      
-    // let editObj = postArr.find(post =>post) 
     let Edit_url = `${base_url}/posts/${editId}`;
     
     spinner.classList.remove('d-none');
@@ -151,8 +154,10 @@ function onEdit(ele){
 
         xhr.setRequestHeader('content-type', 'application/json') ;
         xhr.setRequestHeader('Autho','Get token from');
-
         xhr.send(null);
+        document.querySelectorAll('.btn-outline-danger').forEach(btn=>{
+            btn.disabled=true;
+        });
         
         xhr.onload =function (){ 
           if(xhr.status>=200 && xhr.status<=299){
@@ -166,6 +171,10 @@ function onEdit(ele){
                 addpost.classList.add('d-none'); 
                 updateBtn.classList.remove('d-none');  
                 spinner.classList.add('d-none')
+                 postform.scrollIntoView({
+                  behavior: 'smooth',
+                  block: 'start'
+              });
               }else{ 
                  snackbar('Data is not patched', 'error');
                 spinner.classList.add('d-none')
@@ -192,21 +201,47 @@ function onUpdate(){
 
    xhr.open('PATCH', updateUrl);
    xhr.send(JSON.stringify(updateObj));
-
    xhr.onload = function (){ 
   
       if(xhr.status>=200 && xhr.status<=200){ 
        let res = xhr.response; 
            let col= document.getElementById(updateId); 
-            let h3= col.querySelector('.card-header h3')
-               h3.innerText= updateObj.title;
+              col.innerHTML =` <div class="card h-100">
+                                 <div class="card-header" data-toggle="tooltip" data-placement="top" title="${updateObj.title}">
+                                 <H3>${updateObj.title} </H3>                           
+                                  </div>
+                                   <div class="card-body">
+                                     ${updateObj.body}
+                                    </div>
+                                 <div class="card-footer d-flex justify-content-between">
+                                  <button onclick="onEdit(this)" class="btn btn-inline-block btn-outline-primary">Edit</button>
+                                  <button onclick="onRemove(this)" class="btn btn-inline-block btn-outline-danger">Delete</button>
+                                 </div>
+                                </div>`
            
-            let p= col.querySelector('.card-header p')
-                p.innerText= updateObj.body;   
+                addpost.classList.remove('d-none'); 
+                updateBtn.classList.add('d-none');  
+                spinner.classList.add('d-none') 
+                postform.reset()
+                 document.querySelectorAll('.btn-outline-danger').forEach(btn=>{
+                    btn.disabled=false;
+                });
+                snackbar('updated succesfully', 'success')
+                col.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'center'
+                  });
+
+    col.classList.add('highlight');
+
+    setTimeout(() => {
+        col.classList.remove('highlight');
+    }, 4000);
 
             console.log(res); 
             
        }else{  
+            snackbar('updated failed', 'error')
            spinner.classList.add('d-none');   
        }
     }
@@ -216,4 +251,4 @@ function onUpdate(){
 
 
 postform.addEventListener('submit', onPostSubmit)
- 
+ updateBtn.addEventListener('click', onUpdate)
